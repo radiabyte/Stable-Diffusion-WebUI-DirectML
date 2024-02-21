b1d113e400df3273fc37e7882559ccda57c6770c
radiabyte <72905706+radiabyte@users.noreply.github.com>
2024-02-21 03:22:54 -0800

    Update install.py

--- install.py
+++ install.py
@@ -13,7 +13,7 @@ def install():
     try:
         DML_UNET_MODEL_DIR = os.path.join(paths_internal.models_path, "Unet-dml")
         if not os.path.exists(DML_UNET_MODEL_DIR):
-        os.makedirs(DML_UNET_MODEL_DIR)
+            os.makedirs(DML_UNET_MODEL_DIR)
     except:
         print("Error")
 
